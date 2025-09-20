df0dfb97e8b9954304b38bfbb751c8a8fc78148d
Jackson Brammer <jwbram@gmail.com>
2025-09-20 11:40:49 -0700

    Switch main app to use DashboardView instead of Layout

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Layout from './components/Layout.jsx'
+import DashboardView from './components/DashboardView.jsx'
 import { GameStateProvider } from './state/state.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GameStateProvider>
-      <Layout />
+      <DashboardView />
     </GameStateProvider>
   </React.StrictMode>,
 )
